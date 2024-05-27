73b698b0fa510fc0859036d2c0280ee693b5861a
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2024-05-26 22:00:46 -0400

    added discord colors

--- R/scale_fill_theme.R
+++ R/scale_fill_theme.R
@@ -21,7 +21,8 @@ scale_fill_theme <- function(theme = "nord_aurora", theme_key = list(
   nord_polar_night = c("#2E3440", "#3B4252", "#434C5E", "#4C566A"),
   nord_snow_storm = c("#D8DEE9", "#E5E9F0", "#ECEFF4"),
   nord_frost = c("#8FBCBB", "#88C0D0", "#81A1C1", "#5E81AC"),
-  nord_aurora = c("#BF616A", "#D08770", "#EBCB8B", "#A3BE8C", "#B48EAD")
+  nord_aurora = c("#BF616A", "#D08770", "#EBCB8B", "#A3BE8C", "#B48EAD"),
+  discord = c("#5865F2", "#57F287", "#FEE75C", "#EB459E", "#ED4245")
 )) {
   scale_fill_manual(values = theme_key[[theme]])
 }
