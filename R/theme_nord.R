2e7e94bc3f16a1aadff9470a3af5b27cee088738
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-05-27 18:15:59 -0400

    updated theme_nord and added documentation

--- R/theme_nord.R
+++ R/theme_nord.R
@@ -44,25 +44,29 @@ theme_nord <- function(..., light = TRUE, base_size = 12, font = "Roboto") {
 
   ggplot2::theme(
 
-    plot.margin = ggplot2::margin(10, 10, 10, 10),
+    plot.margin = ggplot2::margin(5, 10, 10, 10, "mm"),
     plot.background = ggplot2::element_rect(fill = background_color, color = background_color),
-    plot.title = ggplot2::element_text(size = title_size, color = title_color, family = font),
-    plot.subtitle = ggplot2::element_text(size = base_size, color = text_color, family = font),
+    plot.title = ggplot2::element_text(size = title_size, color = title_color, family = font, margin = ggplot2::margin(0,0,3,0,"mm")),
+    plot.subtitle = ggplot2::element_text(size = base_size, color = text_color, family = font, margin = ggplot2::margin(2,0,7,0,"mm")),
     plot.caption = ggplot2::element_text(size = base_size, color = text_color, family = font),
 
     panel.background = ggplot2::element_rect(fill = background_color, color = background_color),
-    panel.grid = ggplot2::element_line(color = text_color),
+    panel.grid.major = ggplot2::element_line(color = text_color, linewidth = .1),
+    panel.grid.minor = ggplot2::element_blank(),
 
     strip.background = ggplot2::element_rect(fill = background_color, color = background_color),
     strip.text = ggplot2::element_text(size = base_size, color = text_color, family = font),
 
     axis.title = ggplot2::element_text(size = base_size, color = text_color, family = font),
+    axis.title.x = ggplot2::element_text(margin = ggplot2::margin(5,0,0,0,"mm")),
+    axis.title.y = ggplot2::element_text(margin = ggplot2::margin(0,5,0,0,"mm")),
     axis.text = ggplot2::element_text(size = base_size, color = text_color, family = font),
-    axis.ticks = ggplot2::element_line(color = text_color),
+    axis.ticks = ggplot2::element_blank(),
 
     legend.background = ggplot2::element_rect(fill = background_color, color = background_color),
-    legend.title = ggplot2::element_text(size = base_size, color = text_color, family = font),
+    legend.title = ggplot2::element_text(size = base_size, color = text_color, family = font, margin = ggplot2::margin(0,3,0,0,"mm")),
     legend.text = ggplot2::element_text(size = base_size, color = text_color, family = font),
+    legend.key = ggplot2::element_rect(fill = background_color, color = background_color),
 
     complete = FALSE,
     validate = TRUE,
